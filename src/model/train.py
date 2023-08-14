85a13e1640ff7d7bdb3899c638fde6e12e00acb9
K3nial <96307999+K3nial@users.noreply.github.com>
2023-08-14 14:00:34 +0000

    added split_data with autolog

--- src/model/train.py
+++ src/model/train.py
@@ -5,16 +5,16 @@ import glob
 import os
 
 import pandas as pd
-import numpy as np
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-
+import mlflow
+import numpy as np
 
 # define functions
 def main(args):
     # TO DO: enable autologging
-
+    mlflow.autolog()
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -35,20 +35,29 @@ def get_csvs_df(path):
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
 
-# TO DO: add function to split data
-def split_data()
-    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
-    len(X)
+def split_data(df):
+    # Selecting specific columns as features (X) and target variable (y)
+    X = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure', 'TricepsThickness',
+            'SerumInsulin', 'BMI', 'DiabetesPedigree', 'Age']].values
+    y = df['Diabetic'].values
 
-    print(np.unique(y, return_counts=True))
+    # Print the number of samples in the dataset and the class distribution
+    print("Number of samples:", len(X))
+    print("Class distribution:", np.unique(y, return_counts=True))
 
+    # Split the data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
 
+    # Return the training and testing sets
+    return X_train, X_test, y_train, y_test
 
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
-    LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
+    model = LogisticRegression(C=1/reg_rate, solver="liblinear")
+    model.fit(X_train, y_train)
+
+    # TO DO: Add evaluation and logging code here
 
 
 def parse_args():
@@ -56,10 +65,8 @@ def parse_args():
     parser = argparse.ArgumentParser()
 
     # add arguments
-    parser.add_argument("--training_data", dest='training_data',
-                        type=str)
-    parser.add_argument("--reg_rate", dest='reg_rate',
-                        type=float, default=0.01)
+    parser.add_argument("--training_data", dest='training_data', type=str)
+    parser.add_argument("--reg_rate", dest='reg_rate', type=float, default=0.01)
 
     # parse args
     args = parser.parse_args()
